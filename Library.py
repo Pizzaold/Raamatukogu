8e06b1f4343a6105e14b01b8e3bfbec9a2be73b3
Aaron <aaronalev@gmail.com>
2023-03-23 10:35:49 +0200

    Made a file to hold objects in library and read from the file

--- Library.py
+++ Library.py
@@ -5,10 +5,18 @@ Library class is used to create a library object
 class Library:
     def __init__(self, name):
         self.name = name
-        self.mebmers = []
-        self.rentables = []
+        self.members = []
+        self.rentables = open("rentable_objects", "r").readlines()
 
     def add_library(self, name):
         f = open("Library.txt", "a")
         f.write(self.name)
         f.close()
+
+    def test(self):
+        print(self.rentables)
+
+
+if __name__ == "__main__":
+    library1 = Library("Test")
+    library1.test()
