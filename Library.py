d112053e9eacf2dc8aa34ad2789fb36ad6322c4b
Aaron <aaronalev@gmail.com>
2023-03-27 13:54:19 +0300

    Made the rented object amount reduce by 1 when rented + some other stuff

--- Library.py
+++ Library.py
@@ -2,6 +2,7 @@
 Library class is used to create a library object
 """
 from datetime import date
+from Objects import Book
 
 class Library:
     def __init__(self, name):
@@ -17,28 +18,45 @@ class Library:
     def test(self):
         print(self.rentables)
 
-    def rent(self, rentable_name, user):
+    def rent(self, rentable, user):
         """Rent out a rentable object to user by adding it to members lib and removing one from library lib."""
         items = open("rentable_objects", "r+")
         offset = 0
         for item in items:
             item_name = item.split(",")[1]  # Find the name of the object
-            if rentable_name == item_name:
-                with open("members.txt", "r") as temp:  # Opens members for reading
+            if rentable.name == item_name:
+                with open("members.txt", "r") as temp:  # Opens members.txt for reading
                     lines = temp.readlines()
-                with open("members.txt", "w") as members:  # Opens members for writing
+                with open("members.txt", "w") as members:  # Opens members.txt for writing
                     for line in lines:
                         members.write(line)  # Writes line back to file
                         offset += len(line)  # Keep count of lines
                         if user in line:
                             line.strip("\n")
                             members.seek(offset - 1)  # Seek out the right place to write the new line
-                            members.write(f",{rentable_name};{date.today()}\n")  # Write the rented object to user lib
+                            members.write(f",{rentable};{date.today()}\n")  # Write the rented object to user lib
+                with open("rentable_objects", "r") as temp:  # Open rentable_objects.txt for reading
+                    lines = temp.readlines()
+                with open("rentable_objects", "w") as objects:  # Open rentable_objects.txt for writing
+                    for line in lines:
+                        amount = line.split(",")[-1:]  # Finds the amount of available items
+                        for item in amount:  # Makes list into str
+                            amount = item
+                        new_amount = int(amount) - 1  # New amount
+                        if rentable.name in line:
+                            temp = line[:-1] + str(new_amount)  # New line
+                            objects.write(temp)  # Writes edited amount back to file
+                        else:
+                            objects.write(line)  # Writes unedited lines back to file
+
+
+
 
 
 
 
 if __name__ == "__main__":
+    book1 = Book("Lord of the rings")
     library1 = Library("Test")
     library1.test()
-    library1.rent("Lord of the rings", "aimar")
+    library1.rent(book1, "aimar")
---
+++ Objects.py
@@ -0,0 +1,41 @@
+class Book:
+
+    def __init__(self, name):
+        self.name = name
+        rentable = True
+
+    def add(self, amount):
+        with open("rentable_objects", "w") as objects:
+            objects.write(f"book,{str(self)},{amount}")
+
+    def __repr__(self):
+        return self.name
+
+
+class Magazine:
+
+    def __init__(self, name):
+        self.name = name
+        rentable = True
+
+    def __repr__(self):
+        return self.name
+
+
+class DVD:
+
+    def __init__(self, name):
+        self.name = name
+        rentable = True
+
+    def __repr__(self):
+        return self.name
+
+
+if __name__ == "__main__":
+    book1 = Book("Lord of the rings")
+    book1.add(2)
+
+with open("rentable_objects", "r") as object1:
+    line = object1.readline()
+    print(line)
