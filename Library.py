1959bea97f4e722d26304d94d75bffaae23a1caf
Aaron <aaronalev@gmail.com>
2023-03-27 12:33:35 +0300

    Made the rent function be able to write rented books to file

--- Library.py
+++ Library.py
@@ -1,11 +1,12 @@
 """
 Library class is used to create a library object
 """
+from datetime import date
 
 class Library:
     def __init__(self, name):
         self.name = name
-        self.members = open("members", "r").readlines()[1:]
+        self.members = open("members.txt", "r").readlines()[1:]
         self.rentables = open("rentable_objects", "r").readlines()
 
     def add_library(self, name):
@@ -16,15 +17,28 @@ class Library:
     def test(self):
         print(self.rentables)
 
-    def rent(self):
-        lines = open("members", "r")
-        for line in lines:
-            line.strip("\n")
-            print(line)
+    def rent(self, rentable_name, user):
+        """Rent out a rentable object to user by adding it to members lib and removing one from library lib."""
+        items = open("rentable_objects", "r+")
+        offset = 0
+        for item in items:
+            item_name = item.split(",")[1]  # Find the name of the object
+            if rentable_name == item_name:
+                with open("members.txt", "r") as temp:  # Opens members for reading
+                    lines = temp.readlines()
+                with open("members.txt", "w") as members:  # Opens members for writing
+                    for line in lines:
+                        members.write(line)  # Writes line back to file
+                        offset += len(line)  # Keep count of lines
+                        if user in line:
+                            line.strip("\n")
+                            members.seek(offset - 1)  # Seek out the right place to write the new line
+                            members.write(f",{rentable_name};{date.today()}\n")  # Write the rented object to user lib
+
 
 
 
 if __name__ == "__main__":
     library1 = Library("Test")
     library1.test()
-    library1.rent()
+    library1.rent("Lord of the rings", "aimar")
