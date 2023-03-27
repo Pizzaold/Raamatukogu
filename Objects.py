20b5b0d05773c1be7359e3b097c50659e6682f00
Knack <Aimarkiivits1@gmail.com>
2023-03-27 11:24:16 +0300

    Made three objects and gave them the rentable property.

---
+++ Objects.py
@@ -0,0 +1,28 @@
+class Book:
+
+    def __init__(self, name):
+        self.name = name
+        rentable = True
+
+    def __repr__(self):
+        return self.name
+
+
+class Magazine:
+
+    def __init__(self, name):
+        self.name = name
+        rentable = True
+
+    def __repr__(self):
+        return self.name
+
+
+class DVD:
+
+    def __init__(self, name):
+        self.name = name
+        rentable = True
+
+    def __repr__(self):
+        return self.name
